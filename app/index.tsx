9973d04691d00e11c58b8627c19d5fb212b87ee2
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-29 15:48:30 -0400

    update

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>First Line of Code.</Text>
+      <Text> Second Line of Code</Text>
+    </View>
+  );
+}
